8bea2fe7ddfbe835d21bc7ae592dff1e5846c36f
AnaMS205 <anastasia.sexson@icloud.com>
2026-08-28 14:47:09 -0500

    roomSpawner edit

--- scenes/RoomSpawner.gd
+++ scenes/RoomSpawner.gd
@@ -1,11 +1,8 @@
 extends CSGBox3D
-@onready var thing = $"."
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var scene = load("res://Player.tscn")
-	var player = scene.instance()
-	add_child(player)
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
