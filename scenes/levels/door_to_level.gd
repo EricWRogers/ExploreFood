1100eaf28070e8f6d25becc5b01a3442fa035277
Nathan <nathanielorozco42@gmail.com>
2026-08-29 15:07:15 -0500

    Level Change Quickfix
    Connected signal to door

--- scenes/levels/door_to_level.gd
+++ scenes/levels/door_to_level.gd
@@ -1,6 +1,7 @@
 extends Area3D
 
-@export var inital_scene: StringName = &"uid://dgrvt1p5mi8fr"
+@export var inital_scene: StringName = &"uid://c6e1k24uvewn0"
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -12,6 +13,7 @@ func _process(delta: float) -> void:
 
 
 func _on_body_entered(body):
+	print("Entering")
 	if body.is_in_group("Player"):
 		SceneLoader.load_scene(inital_scene)
 		
