2c3cb65a86b2cab20b2bc23dc021d1d873a17412
Nathan <nathanielorozco42@gmail.com>
2026-08-29 14:19:41 -0500

    Deleting Old Scripts

--- scripts/test_scripts/ingredient_test_spawn.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-@onready var spawn_point: Marker3D = $spawn_point
-var spawn_apple = preload("res://scenes/prefabs/Ingredients/food.tscn")
-
-func _ready() -> void:
-	var instance = spawn_apple.instantiate()
-	add_child(instance)
-	instance.position = spawn_point.position
