79bef91a810742185d9eeead58accb4ab37e58e4
Nathan <nathanielorozco42@gmail.com>
2026-08-30 13:55:28 -0500

    Portal Added to Terrain Scene, Player Grouping Fixed

--- scripts/RoomScripts/Level.gd
+++ scripts/RoomScripts/Level.gd
@@ -3,6 +3,6 @@ extends Node
 @export var level_name : String = "level"
 signal level_changed(level_name)
 
-func _on_area_3d_body_entered(body: Node3D) -> void:
+func _on_portal_body_entered(body: Node3D) -> void:
 	if body.is_in_group("Player"):
 		emit_signal("level_changed", level_name)
--- scripts/RoomScripts/floor_boundary.gd
+++ scripts/RoomScripts/floor_boundary.gd
@@ -2,4 +2,4 @@ extends Area3D
 
 func _on_body_entered(body: Node3D) -> void:
 	if body.is_in_group("Player"):
-		body.position.y += 100
+		body.position = Vector3(0, 10, 0)
