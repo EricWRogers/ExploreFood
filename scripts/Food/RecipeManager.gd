883252e9ed7501f6ddb58edd9c05e204309dc738
Nathan <nathanielorozco42@gmail.com>
2026-08-30 22:14:03 -0500

    Recipe System!!! + Cauldron added to kitchen scene

--- scripts/Food/RecipeManager.gd
+++ scripts/Food/RecipeManager.gd
@@ -1,17 +1,18 @@
 extends StaticBody3D
 
 #var cheesecake : Array #array of food types
-var recipes : Array
+@export var recipes : Array[Resource]
 var food_types : Array
 
 #list of all recipes
 #list of current foods in array
 
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("create_icon"):
+		find_recipe()
 
 #sort through all the foods in current food array
 #for each food in food array, whittle down list of recipes
@@ -23,24 +24,28 @@ func _on_area_3d_body_entered(body: Node3D) -> void:
 		food_types.append(body.category)
 		body.queue_free()
 		print(food_types)
+		
 	#if item in area is an ingredient
 
 func find_recipe():
 	var current_recipe
-	
-	#var ing_to_look = item #get item
-	#var ing_ftype = ing_to_look.foodType
-	
-	#for item in items_in_pot:
+	var chosen
+	for item in recipes: 
+		for i_types in food_types: 
+			if i_types in item.ingredients:
+				current_recipe = item
+				print(current_recipe)
+			else:
+				current_recipe = null
+			continue
+		if current_recipe:
+			print(current_recipe)
+			print("make chicken")
+			chosen = current_recipe
+			return chosen
+		else:
+			print("dubious")
+			chosen = "Dubious"
+			return chosen
 		
-		#default result is dubious food
-		
-		#for loop i in recipe list 
-			#loop through each key in recipe
-				#if ing_to_look = food type
-					#add to list of potential recipes
-					#break
-				#else
-					#go to next
-		#match ing_to_look:
-			#pass
+	return chosen
