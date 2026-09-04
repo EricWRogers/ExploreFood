8ec2c05eecc9643f3ffdf2139d38d81a59d92541
kinteyoi <calebazlin5@gmail.com>
2026-09-04 00:22:41 -0500

    kitchen has effects

--- scripts/recipe_system/RecipeManager.gd
+++ scripts/recipe_system/RecipeManager.gd
@@ -3,6 +3,7 @@ extends StaticBody3D
 @export var lvl_manager: Node
 @export var recipes : Array[Resource]
 var food_types : Array
+var cooking = false
 
 #list of all recipes
 #list of current foods in array
@@ -11,8 +12,8 @@ func _ready() -> void:
 	lvl_manager = $"../.."
 
 func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("create_icon"):
-		find_recipe()
+	if Input.is_action_just_pressed("cook"):
+		start_cooking()
 
 #sort through all the foods in current food array
 #for each food in food array, whittle down list of recipes
@@ -22,10 +23,23 @@ func _process(_delta: float) -> void:
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.has_method("RollSpawn"):
 		food_types.append(body.category)
+		update_held_item(body.id)
 		body.queue_free()
 		print(food_types)
 		
 	#if item in area is an ingredient
+	
+func start_cooking():
+	if cooking == true:
+		return
+	if food_types.is_empty():
+		return
+	cooking = true
+	$StaticBody3D.set_collision_layer_value(1, true)
+	$StaticBody3D/PotLid.show()
+	$Timer.start()
+	$Cooking.play("Cooking")
+	pass
 
 func find_recipe():
 	var current_recipe
@@ -66,3 +80,42 @@ func find_recipe():
 	current_recipe = null
 	chosen_recipe = null
 	return
+	
+func update_held_item(id):
+	match id:
+		0:
+			reset_held()
+		1:
+			$FoodRot/Terry.show()
+		2:
+			$FoodRot/Waffle.show()
+		3:
+			$FoodRot/Frogbutt.show()
+		4:
+			pass
+		5:
+			$FoodRot/Bebeh.show()
+		6:
+			pass
+		7:
+			$FoodRot/DoughBebeh.show()
+		8:
+			$FoodRot/MeatBall.show()
+
+func reset_held():
+	$FoodRot/Frogbutt.hide()
+	$FoodRot/Terry.hide()
+	$FoodRot/Waffle.hide()
+	$FoodRot/Bebeh.hide()
+	$FoodRot/DoughBebeh.hide()
+	$FoodRot/MeatBall.hide()
+
+
+func _on_timer_timeout() -> void:
+	cooking = false
+	find_recipe()
+	reset_held()
+	$Cooking.play("RESET")
+	$StaticBody3D.set_collision_layer_value(1, false)
+	$StaticBody3D/PotLid.hide()
+	$AnimationPlayer.play("RESET")
