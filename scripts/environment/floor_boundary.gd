a3629f4ee2dd092c47799c9705853aeb2797ca14
kinteyoi <calebazlin5@gmail.com>
2026-09-07 17:36:18 -0500

    new level

--- scripts/environment/floor_boundary.gd
+++ scripts/environment/floor_boundary.gd
@@ -1,5 +1,7 @@
 extends Area3D
 
+@export var spawn: Node3D
+
 func _on_body_entered(body: Node3D) -> void:
 	if body.is_in_group("Player"):
-		body.global_position = Vector3(0, 10, 0)
+		body.global_position = spawn.global_position
