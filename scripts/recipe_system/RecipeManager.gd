917e31f08566c7ace64db4018518821880e9aedd
Nathan <nathanielorozco42@gmail.com>
2026-09-08 10:49:02 -0500

    Better Spawn Point for food

--- scripts/recipe_system/RecipeManager.gd
+++ scripts/recipe_system/RecipeManager.gd
@@ -77,7 +77,7 @@ func find_recipe():
 	var recipe_spawn = null
 	recipe_spawn = chosen_recipe.product.instantiate()
 	current_scene.add_child(recipe_spawn)
-	recipe_spawn.position = Vector3(-4.1,1.7,0)
+	recipe_spawn.position = Vector3(-1,1.7,-6)
 	
 	#empty list for next batch
 	food_types.clear()
