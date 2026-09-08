fc802c07266ec76228a7a8d4e86e6d52c1a17c10
kinteyoi <calebazlin5@gmail.com>
2026-09-08 00:22:02 -0500

    fixing portal graphics in levels

--- scripts/level_manager/portal.gd
+++ scripts/level_manager/portal.gd
@@ -7,8 +7,11 @@ extends Area3D
 @onready var mat = $Portalmesh.get_surface_override_material(0)
 
 func _ready() -> void:
+	
 	if !active:
 		self.set_collision_layer_value(1, false)
+		self.set_collision_mask_value(1, false)
 		$Portalmesh.hide()
 		$OmniLight3D.hide()
+	$OmniLight3D.light_color = light_color
 	mat.set_shader_parameter("albedo_texture", image_of_world)
