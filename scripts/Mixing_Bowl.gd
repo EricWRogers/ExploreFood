92aa00268439af212d33c4a57d1694f27935af0b
Crow <crowmiller6@gmail.com>
2026-09-24 19:03:24 -0500

    random number button

--- scripts/Mixing_Bowl.gd
+++ scripts/Mixing_Bowl.gd
@@ -5,9 +5,13 @@ extends Node2D
 var following := false
 const MAX_DIST := 225
 var count : int = 0 
+var rng = RandomNumberGenerator.new()
+var my_random_number : int
 
 func _ready() -> void:
 	$"../Button/ColorIndicator".modulate = Color (1, 0, 0)
+	my_random_number = rng.randi_range(1, 5)
+	print(my_random_number)
 
 func _physics_process(delta: float) -> void:
 	var mouseDist := get_global_mouse_position().distance_to( $knob.global_position )
@@ -33,10 +37,10 @@ func _physics_process(delta: float) -> void:
 
 func _on_button_pressed() -> void:
 	count += 1
-	if count == 5:
+	if count == my_random_number:
 		print("yum")
 		$"../Button/ColorIndicator".modulate = Color (0, 1, 0)
-	elif count > 5:
+	elif count > my_random_number:
 		count = 1
 		$"../Button/ColorIndicator".modulate = Color (1, 0, 0)
 	print(count)
